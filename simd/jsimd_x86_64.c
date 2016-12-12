eb1b15218371be66fd9be6fb25bc5609eb0d10d5
Sergej <sergej@gnedo.com>
2016-12-12 14:48:33 +0100

    Fixed build issue for x64 platform

--- simd/jsimd_x86_64.c
+++ simd/jsimd_x86_64.c
@@ -49,10 +49,14 @@ init_simd (void)
   simd_support = JSIMD_SSE2 | JSIMD_SSE;
 
   /* Force different settings through environment variables */
+#ifndef WINRT
   env = getenv("JSIMD_FORCENONE");
+#endif
   if ((env != NULL) && (strcmp(env, "1") == 0))
     simd_support = 0;
+#ifndef WINRT
   env = getenv("JSIMD_NOHUFFENC");
+#endif
   if ((env != NULL) && (strcmp(env, "1") == 0))
     simd_huffman = 0;
 }
